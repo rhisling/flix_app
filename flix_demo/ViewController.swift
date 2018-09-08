4e2e20093b87ed5ceef06a0456342dd7c4225093
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-09-07 17:00:45 -0700

    Initial Commit

---
+++ flix_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix_demo
+//
+//  Created by Aravindh Elayakumar on 9/7/18.
+//  Copyright © 2018 Aravindh Elayakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
